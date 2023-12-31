e52c6f8e0ae03224212624b48b489695b9141582
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 09:49:25 +0530

    map(Function)     */

--- src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
@@ -1,8 +1,5 @@
 package com.lakshmi.collectors;
 
-import com.lakshmi.modal.EmpIMPL;
-import com.lakshmi.modal.Employee;
-
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,20 +9,19 @@ public class MethodsInCollectors {
 
     public static void main(String[] args) {
 
-    // 1.Creating list: toList() , toSet(),
-
+        // 1.Creating list: toList() , toSet(),
         var list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         var collect = list.stream().map(n -> n * n).collect(Collectors.toList());
-        var set = list.stream().map(n -> n+n).collect(Collectors.toSet());
-        System.out.println(list.getClass()+" "+list);
+        var set = list.stream().map(n -> n + n).collect(Collectors.toSet());
+        System.out.println(list.getClass() + " " + list);
         System.out.println("*************");
-        System.out.println(collect.getClass()+" "+collect);
+        System.out.println(collect.getClass() + " " + collect);
         System.out.println("**************");
-        System.out.println(set.getClass()+" "+set);
+        System.out.println(set.getClass() + " " + set);
 
 
-    // 2. Creating specific collection: toCollection()
-        var collect1 = list.parallelStream().filter(n -> n > 2).collect(Collectors.toList(LinkedList::new));
-        System.out.println(collect1.getClass());
+        // 2. Creating specific collection: toCollection(),
+        var list1 = Arrays.asList("a", "b", "c", "d", "e", "f");
+        list.parallelStream().map(s -> s.intValue()).collect(Collectors.toList()).forEach(System.out::println);
     }
 }
