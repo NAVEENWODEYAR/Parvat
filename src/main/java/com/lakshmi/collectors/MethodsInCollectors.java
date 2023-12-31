c75ae212687b6454fc80b143a8cf69bcd295f71a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 11:11:25 +0530

    Creating specific collection: toCollection()

--- src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
@@ -1,8 +1,6 @@
 package com.lakshmi.collectors;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MethodsInCollectors {
@@ -21,7 +19,8 @@ public class MethodsInCollectors {
 
 
         // 2. Creating specific collection: toCollection(),
-        var list1 = Arrays.asList("a", "b", "c", "d", "e", "f");
-        list.parallelStream().map(s -> s.intValue()).collect(Collectors.toList()).forEach(System.out::println);
+            List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8,9);
+            var collect1 = num.stream().filter(n -> n >= 2).collect(Collectors.toCollection(LinkedList::new));
+            System.out.println(collect1);
     }
 }
