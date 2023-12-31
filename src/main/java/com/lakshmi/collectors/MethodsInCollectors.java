b94fdd2fb1977851c97e826db3e9b43acf1e41bb
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 09:42:39 +0530

    java: method toList in class java.util.stream.Collectors cannot be applied to given types;

--- src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
@@ -4,6 +4,7 @@ import com.lakshmi.modal.EmpIMPL;
 import com.lakshmi.modal.Employee;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,20 @@ public class MethodsInCollectors {
 
     public static void main(String[] args) {
 
-    // 1.Creating list: toList()
-        Employee e = new Employee();
-        System.out.print(e.getId());
+    // 1.Creating list: toList() , toSet(),
 
         var list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         var collect = list.stream().map(n -> n * n).collect(Collectors.toList());
-        System.out.print(list.getClass()+" "+list);
+        var set = list.stream().map(n -> n+n).collect(Collectors.toSet());
+        System.out.println(list.getClass()+" "+list);
         System.out.println("*************");
         System.out.println(collect.getClass()+" "+collect);
+        System.out.println("**************");
+        System.out.println(set.getClass()+" "+set);
+
+
+    // 2. Creating specific collection: toCollection()
+        var collect1 = list.parallelStream().filter(n -> n > 2).collect(Collectors.toList(LinkedList::new));
+        System.out.println(collect1.getClass());
     }
 }
