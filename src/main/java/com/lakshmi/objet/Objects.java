3b42f71c61f0c3f594ae8496f419682210c212d1
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-03 23:00:05 +0530

    logOut

---
+++ src/main/java/com/lakshmi/objet/Objects.java
@@ -0,0 +1,10 @@
+package com.lakshmi.objet;
+
+public class Objects {
+    public static void main(String[] args) {
+        var objects = new Objects();
+        var objects1 = new Objects();
+
+        System.out.println(objects+""+objects1);
+    }
+}
