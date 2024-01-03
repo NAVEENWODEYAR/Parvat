98381cefd033bc6be4e2eb866cc5079abeaf8d27
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-03 23:09:32 +0530

    object assignment

--- src/main/java/com/lakshmi/objet/Objects.java
+++ src/main/java/com/lakshmi/objet/Objects.java
@@ -3,7 +3,7 @@ package com.lakshmi.objet;
 public class Objects {
     public static void main(String[] args) {
         var objects = new Objects();
-        var objects1 = new Objects();
+        var objects1 = objects;
 
         System.out.println(objects+""+objects1);
     }
