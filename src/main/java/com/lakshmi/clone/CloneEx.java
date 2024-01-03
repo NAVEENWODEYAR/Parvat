7065057543367472722663406b55db9df5b055b7
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-03 22:47:45 +0530

    first commit

--- src/main/java/com/lakshmi/clone/CloneEx.java
+++ src/main/java/com/lakshmi/clone/CloneEx.java
@@ -1,7 +1,9 @@
 package com.lakshmi.clone;
 
 public class CloneEx implements Cloneable{
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws CloneNotSupportedException {
+        CloneEx ce = new CloneEx();
+        CloneEx ce1 = (CloneEx) ce.clone();
+        System.out.println(ce+" "+ce1);
     }
 }
