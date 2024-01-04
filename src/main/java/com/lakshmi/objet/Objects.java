1e839572a5888c1ef91352c8e2b02f66f1d1b23c
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-04 09:10:06 +0530

    GM

--- src/main/java/com/lakshmi/objet/Objects.java
+++ src/main/java/com/lakshmi/objet/Objects.java
@@ -1,10 +1,15 @@
 package com.lakshmi.objet;
 
+
 public class Objects {
     public static void main(String[] args) {
         var objects = new Objects();
         var objects1 = objects;
 
+        var a = 9;
+        var b = a;
+
         System.out.println(objects+""+objects1);
+        System.out.println(a+""+b);
     }
 }
