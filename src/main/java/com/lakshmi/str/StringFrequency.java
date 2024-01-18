c218c2445d8385e4b071390bd488ee5fa394f576
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-18 08:31:02 +0530

    string frequency using hashmap

--- src/main/java/com/lakshmi/str/StringFrequency.java
+++ src/main/java/com/lakshmi/str/StringFrequency.java
@@ -1,4 +1,18 @@
 package com.lakshmi.str;
 
+import java.util.HashMap;
+
 public class StringFrequency {
+static void stringFrequency(String st){
+    char[] st1 = st.toLowerCase().toCharArray();
+    HashMap<Character, Integer> hmap = new HashMap<>();
+
+    for (char c : st1) {
+        if (hmap.containsKey(c)) {
+            hmap.get(c), hmap.get(c)+1)
+        }
+        else
+            hmap.get(c),1);
+    }
+}
 }
