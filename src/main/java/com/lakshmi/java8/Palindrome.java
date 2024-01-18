bae403aa9f27104ec56ef16d9c5c8bd6e46ff053
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-18 23:13:38 +0530

    palindrome code copied

--- src/main/java/com/lakshmi/java8/Palindrome.java
+++ src/main/java/com/lakshmi/java8/Palindrome.java
@@ -1,10 +1,15 @@
 package com.lakshmi.java8;
 
+import java.util.stream.IntStream;
+
 // Program for palindrome in java8,
 public class Palindrome {
-    static boolean ispalindrome(String dt){
+    static boolean ispalindrome(String str){
 
-        return true;
+        String cleanedStr = str.replaceAll("\\s+", "").toLowerCase();
+        return IntStream.range(0, cleanedStr.length() / 2)
+                .noneMatch(i -> cleanedStr.charAt(i) != cleanedStr.charAt(cleanedStr.length() - i - 1));
+
     }
     public static void main(String[] args) {
         System.out.println("Palindrome using Java8 library");
