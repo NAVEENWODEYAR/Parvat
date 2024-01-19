1a8e9acb5dd550cb9f20cd4ab44ce326118417ca
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-19 22:57:58 +0530

    str.matches(.*[].*)

---
+++ src/main/java/com/lakshmi/str/PatternMatching.java
@@ -0,0 +1,14 @@
+package com.lakshmi.str;
+
+public class PatternMatching {
+    static boolean  isVowelPreset(String st){
+        return st.toLowerCase().matches(".*[aeiou].*");
+    }
+    public static void main(String[] args) {
+        if(isVowelPreset("crypt"))
+            System.out.println("Vowel is present");
+        else
+            System.out.println("not present");
+    }
+}
+
