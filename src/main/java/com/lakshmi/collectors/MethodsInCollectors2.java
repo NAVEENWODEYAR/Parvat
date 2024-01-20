3825466939a19eaa8503eceeb5db407deeeada67
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-20 21:29:00 +0530

    toList()

--- src/main/java/com/lakshmi/collectors/MethodsInCollectors2.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors2.java
@@ -8,6 +8,7 @@ public class MethodsInCollectors2 {
     public static void main(String[] args) {
 
         // 1.Creating list: toList() , toSet(),
-
+        var list = Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight");
+            list.parallelStream().collect(Collectors.toList()).forEach(System.out::println);
     }
 }
