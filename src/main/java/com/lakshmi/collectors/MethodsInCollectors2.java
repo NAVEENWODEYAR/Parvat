5362afac16e2fc0e128ba854a5fc0c4a306c32b8
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-20 21:44:16 +0530

    toCollection() for specific collection

--- src/main/java/com/lakshmi/collectors/MethodsInCollectors2.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors2.java
@@ -6,9 +6,13 @@ import java.util.stream.Collectors;
 public class MethodsInCollectors2 {
 
     public static void main(String[] args) {
+        var list = Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight");
 
         // 1.Creating list: toList() , toSet(),
-        var list = Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight");
             list.parallelStream().collect(Collectors.toList()).forEach(System.out::println);
+
+        // 2. Creating specific collection: toCollection(),
+        var linkedList = list.stream().map(st -> st.toUpperCase()).collect(Collectors.toCollection(LinkedList::new));
+        System.out.println(linkedList);
     }
 }
