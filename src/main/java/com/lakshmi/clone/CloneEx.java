94c742c35cd1502ea1275806e3c0f539d1e35046
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-21 19:22:25 +0530

    cleaning the directory

--- src/main/java/com/lakshmi/clone/CloneEx.java
+++
@@ -1,9 +0,0 @@
-package com.lakshmi.clone;
-
-public class CloneEx implements Cloneable{
-    public static void main(String[] args) throws CloneNotSupportedException {
-        CloneEx ce = new CloneEx();
-        CloneEx ce1 = (CloneEx) ce.clone();
-        System.out.println(ce+" "+ce1);
-    }
-}
