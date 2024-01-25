218da5cab480557a5cdfe0384d3b45584741136a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-25 23:21:19 +0530

    multithreading environments

---
+++ src/main/java/com/lakshmi/threads/ConcurrentHM.java
@@ -0,0 +1,9 @@
+package com.lakshmi.threads;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHM {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String,Integer> chm = new ConcurrentHashMap<String,Integer>();
+    }
+}
