bc41c810918e8c4f36cb5a939d4695b2b1791746
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-25 23:28:09 +0530

    multithreading environments uses seperate dataStructures

--- src/main/java/com/lakshmi/threads/ConcurrentHM.java
+++ src/main/java/com/lakshmi/threads/ConcurrentHM.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentHM {
     public static void main(String[] args) {
-        ConcurrentHashMap<String,Integer> chm = new ConcurrentHashMap<String,Integer>();
+        ConcurrentHashMap<String,Integer> chm = new ConcurrentHashMap<>();
+                                          chm.put("foo",3);
+                                          chm.put("foo",1);
+                                          chm.put("foo",2);
+        System.out.println(chm);
     }
 }
