7934eb3c396d1d1d40398efc28baac9c782b6db0
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-17 16:03:44 +0530

    Thread created

--- src/main/java/com/lakshmi/threads/ConcurrentHM.java
+++ src/main/java/com/lakshmi/threads/ConcurrentHM.java
@@ -2,12 +2,13 @@ package com.lakshmi.threads;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ConcurrentHM {
+public class ConcurrentHM extends Thread{
     public static void main(String[] args) {
         ConcurrentHashMap<String,Integer> chm = new ConcurrentHashMap<>();
                                           chm.put("foo",3);
                                           chm.put("foo",1);
                                           chm.put("foo",2);
         System.out.println(chm);
+        new ConcurrentHM().start();
     }
 }
