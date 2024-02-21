04f495afca59cce1704160a7698ea22b76bd2070
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-21 22:34:19 +0530

    Collectors methods

--- src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors.java
@@ -12,9 +12,9 @@ public class MethodsInCollectors {
         var collect = list.stream().map(n -> n * n).collect(Collectors.toList());
         var set = list.stream().map(n -> n + n).collect(Collectors.toSet());
         System.out.println(list.getClass() + " " + list);
-        System.out.println("*************");
+        System.out.println("*************\n");
         System.out.println(collect.getClass() + " " + collect);
-        System.out.println("**************");
+        System.out.println("**************\n");
         System.out.println(set.getClass() + " " + set);
 
 
@@ -27,16 +27,16 @@ public class MethodsInCollectors {
 
         // 3. Counting elements: Counting(),
             var count = num.parallelStream().collect(Collectors.counting());
-             System.out.println("Total elements present in the collection "+count+"  "+num.stream().count());
+             System.out.println("Total elements present in the collection:"+count+"  "+num.stream().count());
 
         // 4. Finding minimum value: minBy(),
             var first = num.stream().min(Comparator.naturalOrder()).stream().findFirst();
             var i = num.stream().collect(Collectors.minBy(Comparator.reverseOrder())).get();
-            System.out.println("Minimum value in the collection "+first.get()+" "+i);
+            System.out.println("Minimum value in the collection:"+first.get()+" "+i);
 
         // 5. Find the maximum value,
             var i1 = num.parallelStream().collect(Collectors.maxBy(Comparator.naturalOrder())).get();
-            System.out.println("Maximum value in the list "+i1);
+            System.out.println("Maximum value in the list:"+i1);
 
         // 6. Partitioning a list: partitioningBy(),
              var collect3 = num.stream().collect(Collectors.partitioningBy(n -> n > 2));
--- src/main/java/com/lakshmi/collectors/MethodsInCollectors2.java
+++ src/main/java/com/lakshmi/collectors/MethodsInCollectors2.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class MethodsInCollectors2 {
 
     public static void main(String[] args) {
-        var list = Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight");
+        var list = Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight","Nine");
         List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
         // 1.Creating list: toList() , toSet(),
