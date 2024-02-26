0d99490c388a786d11d2c4828a7d226e3820b54b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-26 21:29:00 +0530

    Prime numbers program

---
+++ src/main/java/com/lakshmi/code/PrimeNumber.java
@@ -0,0 +1,12 @@
+package com.lakshmi.code;
+
+public class PrimeNumber {
+
+    static boolean isPrime(int n){
+
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
