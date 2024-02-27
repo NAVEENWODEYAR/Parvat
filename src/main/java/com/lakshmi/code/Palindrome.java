0a232181da6900909940698b82260be7960af17e
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-27 09:06:41 +0530

    palindrome string

--- src/main/java/com/lakshmi/code/Palindrome.java
+++ src/main/java/com/lakshmi/code/Palindrome.java
@@ -3,12 +3,21 @@ package com.lakshmi.code;
 public class Palindrome {
 
     static boolean isPalindrome(String st){
-
-        return false;
+        String str = st.toLowerCase();
+        int left = 0;
+        int right = st.length()-1;
+        while (left<right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        if (isPalindrome("malayalam"))
+        if (isPalindrome("Malayalam"))
             System.out.println("Palindrome");
         else
             System.out.println("Not palindrome");
