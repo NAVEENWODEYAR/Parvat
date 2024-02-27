b4f07a9977b22b0660667d3d409065d09a7529b3
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-27 09:01:44 +0530

    Connected to the target VM, address: '127.0.0.1:52788', transport: 'socket'

--- src/main/java/com/lakshmi/code/Palindrome.java
+++ src/main/java/com/lakshmi/code/Palindrome.java
@@ -2,6 +2,11 @@ package com.lakshmi.code;
 
 public class Palindrome {
 
+    static boolean isPalindrome(String st){
+
+        return false;
+    }
+
     public static void main(String[] args) {
         if (isPalindrome("malayalam"))
             System.out.println("Palindrome");
