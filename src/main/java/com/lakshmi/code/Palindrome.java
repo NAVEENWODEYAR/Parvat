d0614b778c3b72934d75b4c6e0ba0c9837fc69d7
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-27 08:48:20 +0530

    logIn

--- src/main/java/com/lakshmi/code/Palindrome.java
+++ src/main/java/com/lakshmi/code/Palindrome.java
@@ -2,21 +2,6 @@ package com.lakshmi.code;
 
 public class Palindrome {
 
-    static boolean isPalindrome(String st){
-        st.toLowerCase();
-        int left = 0;
-        int right = st.length()-1;
-
-        while (left <right) {
-            if(st.charAt(left) != st.charAt(right)){
-                return false;
-            }
-            left ++;
-            right --;
-        }
-            return true;
-    }
-
     public static void main(String[] args) {
         if (isPalindrome("malayalam"))
             System.out.println("Palindrome");
--- src/main/java/com/lakshmi/code/PrimeNumber.java
+++ src/main/java/com/lakshmi/code/PrimeNumber.java
@@ -1,15 +1,5 @@
 package com.lakshmi.code;
 
-public class PrimeNumber {
-    static boolean isPrime(int n) {
-        if(n<=1)
-            return false;
-        for(int i=2; i<=Math.sqrt(n); i++) {
-            if(n%i==0)
-                return false;
-        }
-        return true;
-    }
     public static void main(String[] args) {
         if(isPrime(9))
             System.out.println("Prime");
