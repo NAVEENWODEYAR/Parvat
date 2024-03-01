c59fe6d1ed0a325fd27f8a8bee3007cb7a1b95bf
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-01 09:20:37 +0530

    StringIndexOutOfBoundsException

---
+++ src/main/java/com/lakshmi/java8/Palindrome1.java
@@ -0,0 +1,25 @@
+package com.lakshmi.java8;
+
+import java.util.stream.IntStream;
+
+public class Palindrome1 {
+    static boolean isPalindrome(String str){
+        str.toLowerCase();
+        int left=0;
+        int right=str.length()-1;
+
+        while (left<right){
+            if(str.charAt(left) !=str.charAt(right))
+                return false;
+            left++;
+            right++;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        if(isPalindrome("level") || isPalindrome("level"))
+            System.out.println("Given strings,are palindrome");
+        else
+            System.out.println("Not palindrome strings");
+    }
+}
